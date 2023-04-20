adb2a65216adf893af8d858b97c67c73688c122e
Arthur Benware <artpolitic91@gmail.com>
2023-04-19 21:22:45 -0400

    added first two get routes in sinatra

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+
+  get '/bikes' do
+    bikes = Bike.by_lowest_price
+    bikes.to_json
   end
+
+  get '/users/:id' do
+    users = User.all
+    users.to_json
+  end
+  
 
 end
